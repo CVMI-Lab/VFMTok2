6bc7af4ceaa903bf1471b92420c832a578090a9d
yexigufu <jyxc-dz-0100605@June.local>
2026-05-22 12:18:12 +0800

    correct VFMAE compute statistics suit

--- scripts/denoise/compute_stats.sh
+++ scripts/denoise/compute_stats.sh
@@ -3,6 +3,6 @@ export NODE_COUNT=1
 export NODE_RANK=0
 export PROC_PER_NODE=8
 export MASTER_PORT=65331
-/jobutils/scripts/torchrun.sh  \
+scripts/autoregressive/torchrun.sh  \
         compute_stats.py --ae-model AE-16 --image-size 256 --batch-size $2 --embed-dim 32 --z-channels 512     \
         --anno-file imagenet/lmdb/train_lmdb --ae-ckpt DINOv2/tokenizer/vfmae-tokenizer.pt
